69b61eabba788ebf7cea3a49231bb9c132bf1055
sergei45russ <hyivsem@bk.ru>
2020-06-28 20:05:46 +0400

    Upload

---
+++ Drinkclass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksClass
+{
+    class Alcoholic
+    {
+        public int price { get; set; }
+        public double percent { get; set; }
+        public string ingredients { get; set; }
+        public string name { get; set; }
+
+        public void GetAlcoholicInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Цена (в руб): {price}| |Процент алкоголя (В % об): {percent}| |Состав: {ingredients}|");
+        }
+    }
+
+    
+    class NonAlcoholic : Alcoholic
+    {
+        public string type { get; set; }
+
+        public virtual void GetNonAlcoholicInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Цена (в руб): {price}| |Тип: {type}|  |Процент алкоголя (В % об): {percent}| |Состев: {ingredients}|");
+        }
+    }
+}
---
+++ Drinkclass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinksClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alcoholic alcoholic = new Alcoholic();
+            alcoholic.name = "Пиво";
+            alcoholic.price = 56;
+            alcoholic.percent = 3.5;
+            alcoholic.ingredients = "Вода, солод, дрожжи, хмель";
+
+            alcoholic.GetAlcoholicInfo();
+
+            NonAlcoholic nonAlcoholic = new NonAlcoholic();
+            nonAlcoholic.type = "Без алкогольное";
+            nonAlcoholic.name = "Лимонад";
+            nonAlcoholic.ingredients = "Вода, сахар, сок лимона или лимонная кислота";
+            nonAlcoholic.price = 42;
+            nonAlcoholic.percent = 0;
+
+            nonAlcoholic.GetNonAlcoholicInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
